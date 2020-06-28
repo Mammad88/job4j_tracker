90575c514bf4824b930de8eba5b89ce36c16b043
Mammad88 <bruki_mammad@mail.ru>
2020-06-28 17:06:45 +0300

    "1. Singleton. [#149281] - version 2.0"

--- src/test/java/ru/job4j/tracker/singleton/TrackerSingle1Test.java
+++ src/test/java/ru/job4j/tracker/singleton/TrackerSingle1Test.java
@@ -2,15 +2,14 @@ package ru.job4j.tracker.singleton;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertSame;
 
 /**
  * Тестирование 4-х вариантов singleton.
  *
  * @author Bruki Mammad (bruki_mammad@mail.ru)
- * @version $1.0$
- * @since 24.06.2020
+ * @version $2.0$
+ * @since 28.06.2020
  */
 public class TrackerSingle1Test {
 
@@ -18,6 +17,6 @@ public class TrackerSingle1Test {
     public void whenSingletonEnumThenOneInstance() {
         TrackerSingle1 trackerEnum1 = TrackerSingle1.INSTANCE;
         TrackerSingle1 trackerEnum2 = TrackerSingle1.INSTANCE;
-        assertThat(trackerEnum1, is(trackerEnum2));
+        assertSame(trackerEnum1, trackerEnum2);
     }
 }
--- src/test/java/ru/job4j/tracker/singleton/TrackerSingle4Test.java
+++ src/test/java/ru/job4j/tracker/singleton/TrackerSingle4Test.java
@@ -5,6 +5,13 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Тестирование 4-х вариантов singleton.
+ *
+ * @author Bruki Mammad (bruki_mammad@mail.ru)
+ * @version $2.0$
+ * @since 28.06.2020
+ */
 public class TrackerSingle4Test {
 
     @Test
